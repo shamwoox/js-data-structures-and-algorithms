b6e2490aea841399b156a5a07645902b45bcc892
Paul Vu <vudpaul@gmail.com>
2018-12-28 10:27:30 -0500

    factorial.js

---
+++ Recursion Problem Set/factorial.js
@@ -0,0 +1,11 @@
+// Write a function called factorial which accepts a number a returns the factorial of that number.
+
+function factorial(num){
+    if(num === 0) return 1;
+    return num * factorial(num - 1);
+ }
+
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
