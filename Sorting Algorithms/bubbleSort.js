1a74fa455c40a7116ee5dd06335c710401aa2af5
Paul Vu <vudpaul@gmail.com>
2019-01-02 10:07:35 -0500

    improve bubbleSort.js

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -2,8 +2,8 @@
 // using the Bubble Sort algorithm.
 
 let bubbleSort = arr => {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++) {
+    for(let i = arr.length; i > 0; i--) {
+        for(let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
