492d65eb41230ad46436208be62a1c9f0ebecda8
Paul Vu <vudpaul@gmail.com>
2019-01-05 09:39:35 -0500

    Singly_Linked_List.js
    Add unshift and get functions

--- Data Structures/Singly_Linked_List.js
+++ Data Structures/Singly_Linked_List.js
@@ -58,27 +58,65 @@ class SinglyLinkedList {
             console.log("EMPTY");
         }
     }
+
+    shift() {
+        if(this.head) {
+            let temp = this.head;
+            this.head = temp.next;
+            this.length--;
+            if(this.length === 0) {
+                this.tail = null;
+            }
+            return temp.val;
+        } else {
+            return undefined;
+        }
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if(this.head) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) {
+            return null;
+        } else {
+            let currentIndex = this.head;
+            for(let i = 0; i < index; i++) {
+                currentIndex = currentIndex.next;
+            }
+            return currentIndex.val;
+        }
+    }
 }
 
 let list = new SinglyLinkedList();
+console.log("====ORIGINAL LIST====");
 list.push("Paul");
 list.push("James");
 list.push("Thomas");
 list.push("Matthew");
 list.traverse();
-console.log("============");
-console.log(`*${list.pop()} was popped*`);
-console.log("New Linked List");
+console.log("=====================");
+console.log(`** ${list.pop()} popped **`);
+console.log("=====================");
 list.traverse();
-console.log("============");
-console.log(`*${list.pop()} was popped*`);
-console.log("New Linked List");
+console.log("=====================");
+console.log(`** ${list.shift()} shifted **`)
+console.log("=====================");
 list.traverse();
-console.log("============");
-console.log(`*${list.pop()} was popped*`);
-console.log("New Linked List");
+console.log("=====================");
+console.log(`** Unshift Anthony **`);
+console.log("=====================");
+list.unshift("Anthony");
 list.traverse();
-console.log("============");
-console.log(`*${list.pop()} was popped*`);
-console.log("New Linked List");
-list.traverse();
+console.log(`GET INDEX 2: ${list.get(2)}`);
