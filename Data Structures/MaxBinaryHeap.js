8fa969ffbe7b11a76412471b9d7ad54e2737244b
Paul Vu <vudpaul@gmail.com>
2019-01-15 11:28:40 -0500

    MaxBinaryHeap
    Add extractMax function.

--- Data Structures/MaxBinaryHeap.js
+++ Data Structures/MaxBinaryHeap.js
@@ -22,13 +22,57 @@ class MaxBinaryHeap {
             }
         }
     }
+
+    extractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if(leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if(rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)){
+                    swap = rightChildIndex;
+                }
+            }
+
+            if(swap === null) {
+                break;
+            }
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(10);
-heap.insert(30);
-heap.insert(13);
-heap.insert(24);
-heap.insert(100);
-heap.insert(99);
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+console.log(heap.extractMax());
 console.log(heap);
